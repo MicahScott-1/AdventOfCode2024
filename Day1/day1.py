8001ff1eaffa65469e2600f2ebb6c71b6021b531
MicahScott-1 <micahkscott@gmail.com>
2024-12-02 16:36:30 -0600

    main

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,1 +1,39 @@
 #! /usr/bin/env python
+
+# Define the lists and var
+list1 = [7,2,8,6,1,2]
+list2 = [4,6,5,2,4,3]
+listOfDistances = []
+
+# Function Definitions
+def orderList(list):
+    list.sort()
+
+def findDifference(item1, item2):
+    if item1 > item2:
+        return item1 - item2
+    if item2 > item1:
+        return item2 - item1
+    else: #they must be equal
+        return 0
+
+def displayTotalDistance(list):
+    totalDistance = 0
+    for item in list:
+        totalDistance += item
+    print("The total distance is: " + str(totalDistance))
+
+# Doing the calculations 
+orderList(list1)
+orderList(list2)
+print("Ordered lists:")
+print(str(list1) + "\n" + str(list2))
+
+if len(list1) == len(list2):
+    for x in range(0,len(list1)):
+        listOfDistances.append(findDifference(list1[x],list2[x]))
+    displayTotalDistance(listOfDistances)
+else:
+    print("There is an error related to the lengths of\nthe lists that I do not know how to handle.")
+    input("ENTER to quit...")
+    quit()
