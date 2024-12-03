2c89ed6211dcaf8ab6886368b511df55ffa5e9a6
MicahScott-1 <micahkscott@gmail.com>
2024-12-03 09:52:22 -0600

    main

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+# read file splitlines on whitespace
+# check all inc or all dec
+# check non inc or dec more than 3
+
+# Define variables
+totalSafe = 0
+inputList = []
+
+# Define functions
+
+def populateLists(inputFileName):
+    with open(inputFileName, "r") as file:
+        for line in file:
+            inputList.append(int(line.split()))
+
+populateLists('inputFile.txt')
+print()
+# Do operations
